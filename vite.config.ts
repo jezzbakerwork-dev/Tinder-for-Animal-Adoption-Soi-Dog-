4bd75a4c9c24df7d592a7e094c75781c8d2acf52
jezzbakerwork-dev <jezzbakerwork@gmail.com>
2026-03-27 00:16:32 +0000

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+base: "/Tinder-for-Animal-Adoption-Soi-Dog-/",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
